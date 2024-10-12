ee4d57e276b28d31f62bccad5bbb4413d144d4da
karito <karitochocce77@gmail.com>
2024-10-11 22:07:12 -0500

    feat(command): add parameters in CreateFavoriteSourceCommand.java.

--- src/main/java/com/group/catchup/news/domain/model/commands/CreateFavoriteSourceCommand.java
+++ src/main/java/com/group/catchup/news/domain/model/commands/CreateFavoriteSourceCommand.java
@@ -7,13 +7,19 @@
 
 package com.group.catchup.news.domain.model.commands;
 
-public record CreateFavoriteSourceCommand(Long id) {
+public record CreateFavoriteSourceCommand(Long id, String name, String description) {
     public CreateFavoriteSourceCommand{
         if (id == null) {
             throw new IllegalArgumentException("Id cannot be null");
         }
         if (id <= 0) {
             throw new IllegalArgumentException("Id cannot be less than or equal to 0");
+        }
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("Name cannot be null or blank");
+        }
+        if (description == null || description.isBlank()) {
+            throw new IllegalArgumentException("Description cannot be null or blank");
         }
     }
 }
